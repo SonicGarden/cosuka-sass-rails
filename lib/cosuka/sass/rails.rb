94919c595afa2999c3eaafee4338a97345802c35
interu <beautybeast07@gmail.com>
2014-04-08 22:44:24 +0900

    git init

---
+++ lib/cosuka/sass/rails.rb
@@ -0,0 +1,9 @@
+require "cosuka/sass/rails/version"
+
+module Cosuka
+  module Sass
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/cosuka/sass/rails/version.rb
@@ -0,0 +1,7 @@
+module Cosuka
+  module Sass
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
